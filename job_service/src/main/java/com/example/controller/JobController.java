678d15d9d16cfa8ff637bc2133a574d83ce290ca
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-06-20 20:05:59 +0000

    Change the Javalin job_service to using a SpringBoot application

---
+++ job_service/src/main/java/com/example/controller/JobController.java
@@ -0,0 +1,78 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+
+import java.security.Timestamp;
+import java.util.UUID;
+
+import com.example.model.Job;
+import com.example.model.JobRequest;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    private static final int STARTING_MAX_RETRIES = 0;
+    private static final int MAX_RETRIES_LIMIT = 10;
+
+    @PostMapping
+    public ResponseEntity<Job> addJob(JobRequest request) {
+
+        // Calculate the nexttime immediately
+        Timestamp nextUTC = getNextRunTime(Timestap.from(Instant.now()), request.Schedule());
+
+        if (nextUTC == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Could not calcuate next job time");
+        }
+
+        // Create job
+        Job job = new Job(UUID.randomUUID(), request.Schedule(), STARTING_MAX_RETRIES, MAX_RETRIES_LIMIT, nextUTC);
+
+        // String SQL
+        String sql = "INSERT INTO jobs (id, schedule, retriesCount, maxRetries, createdAt, nextRun) VALUES (?, ?, ?, ?, ?, ?)";
+
+        // Setup database connection
+        try(
+            Connection conn = Database.getDataSource().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        ) {
+            stmt.setObject(1, job.JobId());
+            stmt.setObject(2, job.Schedule());
+            stmt.setObject(3, job.retryCount());
+            stmt.setObject(4, job.maxRetries());
+            stmt.setObject(5, job.createdAt());
+            
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted < 1) {
+                System.out.println("Couldn't add to database!");
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Failed to insert job: ", e);
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(job);
+    }
+
+
+
+    public static Timestamp getNextRunTime(Timestamp currentTime, String cronString) {
+
+        CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(UNIX));
+
+        Cron cron = parser.parse(cronString); 
+
+        ZonedDateTime nowUTC = currentTime.toInstant().atZone(ZoneOffset.UTC);
+        ZonedDateTime nextUTC = ExecutionTime.forCron(cron).nextExecution(nowUTC).orElse(null);
+
+        if (nextUTC == null) { 
+            return null;
+        }
+
+        return Timestamp.from(nextUTC.toInstant());
+
+    }
+}
