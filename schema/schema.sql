a4fda9e3a5d79c5fdb78ae0b3a9551d945b05f1b
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-07-08 19:11:33 +0000

    Update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,14 +3,15 @@ CREATE DATABASE IF NOT EXISTS dist_jobs_scheduler;
 CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.jobs(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     schedule STRING NOT NULL, 
+    retriesCount INTEGER NOT NULL DEFAULT 0,
     maxRetries INTEGER NOT NULL DEFAULT 10,
-    nextRun TIMESTAMPTZ NOT NULL 
+    nextRun TIMESTAMPTZ NOT NULL,
+    jobActive TEXT NOT NULL DEFAULT 'active'
 );
 
 CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.history (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     jobId UUID NOT NULL REFERENCES dist_jobs_scheduler.jobs(id) ON DELETE CASCADE,
-    retriesCount INTEGER NOT NULL DEFAULT 0,
     jobStatus TEXT NOT NULL DEFAULT 'pending',
     jobStarted TIMESTAMPTZ NOT NULL,
     jobFinished TIMESTAMPTZ NOT NULL
